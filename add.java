2e801a997c3035b4f1cc22e2d0b79695e67bebb8
shubham6705 <67638121+shubham6705@users.noreply.github.com>
2021-02-28 22:32:20 +0530

    addition file is created

---
+++ add.java
@@ -0,0 +1,17 @@
+class Add
+{
+    public void add(int a,int b)
+    {
+      System.out.println(a+b);
+    }
+    public void add(int a,int b,int c)
+    {
+      System.out.println(a+b+c);
+    }
+  public static void main(String args[])
+  {
+    Add a1=new Add();
+    a1.add(2,3);
+    a2.add(3,2,4);
+  }
+}
